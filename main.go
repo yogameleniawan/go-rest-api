2d6f44c8c302ded51ac3817d9a135f4e5b1bb792
yogameleniawan <yogameleniawan@gmail.com>
2022-03-06 16:57:04 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"		
+)
+
+func helloWorld( c *fiber.Ctx) error {
+	return c.SendString("Hello world")
+}
+
+func main(){
+	app := fiber.New()
+
+	app.Get("/", helloWorld)
+
+	app.Listen(":8000")
+}
