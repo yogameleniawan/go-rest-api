93e101cad36014ed03520d7d88a495405959faff
yogameleniawan <yogameleniawan@gmail.com>
2022-03-06 17:28:14 +0700

    setup gorm migration and fiber route

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"		
+	"fmt"
+	"go-rest-todolist/database"
+	model "go-rest-todolist/models"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func helloWorld( c *fiber.Ctx) error {
+func helloWorld(c *fiber.Ctx) error {
 	return c.SendString("Hello world")
 }
 
-func main(){
+func initDatabase() {
+	var err error
+	dsn := "root:@tcp(127.0.0.1:3306)/go_todo?charset=utf8mb4&parseTime=True&loc=Local"
+	database.DBConn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Database connected!")
+	database.DBConn.AutoMigrate(&model.Todo{})
+	fmt.Println("Migrated DB")
+}
+
+func setupRoute(app *fiber.App) {
+	app.Get("/todos", model.GetTodos)
+}
+
+func main() {
 	app := fiber.New()
-
+	initDatabase()
 	app.Get("/", helloWorld)
-
+	setupRoute(app)
 	app.Listen(":8000")
 }
---
+++ models/todo.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"go-rest-todolist/database"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID        uint   `gorm:"primarykey" json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func GetTodos(c *fiber.Ctx) error {
+	db := database.DBConn
+	var todos []Todo
+	db.Find(&todos)
+	return c.JSON(&todos)
+}
